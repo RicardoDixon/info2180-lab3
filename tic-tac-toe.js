228fc1d26e5c555bb673461f5bdb414e367e0f1b
Ricardo Dixon <rickydixxx@gmail.com>
2020-10-09 12:36:21 -0500

    Edited file

---
+++ tic-tac-toe.js
@@ -0,0 +1,42 @@
+let clickCounter = 0;
+let stateOfLetter = "X";      
+let stateOfGame = [];        
+         
+window.onload = () => {
+    
+    let mainBoard = document.getElementById("board");  
+    let divsOps = mainBoard.querySelectorAll("div"); 
+  
+    for (x = 1; x < divsOps.length + 1; x++){
+        let square = divsOps[x-1];
+        square.classList.add("square");
+        square.id = x;  
+        mouseOperations(square); 
+    }
+}
+
+
+mouseOperations = (square) => {
+    
+    let newGameButton = document.getElementsByClassName("btn")[0]; 
+
+    
+    square.onclick = function(){
+        stateOfGame[square.id - 1] = stateOfLetter; 
+        clickCounter += 1;
+        gamePlay(square.id);  
+    }
+    
+clickOperation = (squareID) => {
+    document.getElementById(squareID).onclick = function(){
+        console.log("Disabled!");
+    } 
+} 
+
+
+
+
+
+
+
+}
